a04022a163226bc41b0df47c02a824b458f72e62
typeryougishiki <typeryougishiki@163.com>
2024-07-19 16:49:39 +0800

    remove fault includes and -dev target

--- xmake.lua
+++ xmake.lua
@@ -8,26 +8,25 @@ set_encodings("utf-8")
 set_languages("c++23")
 
 add_requires("rapidcsv")
-includes("header")
 target("xxse-CsvManager-header")
     set_kind("headeronly")
     add_packages("rapidcsv")
     add_headerfiles("header/CsvManager.h")
     add_headerfiles("header/utils/*.h", {prefixdir = "utils"} )
 
-target("xxse-CsvManager-header-dev")
-    set_kind("headeronly")
-    add_packages("rapidcsv")
-    add_headerfiles("header/CsvManager.h")
-    add_headerfiles("header/utils/*.h", {prefixdir = "utils"})
-    local add_macro_var = function(name,value)
-        add_defines(name.."="..value.."");
-    end
-    local add_macro_var_str = function(name,value)
-        add_defines(name.."=\""..value.."\"");
-    end
-    add_macro_var_str("XXSE_CSV_MANAGER_XXSE_NAME","SFSE");
-    add_macro_var_str("XXSE_CSV_MANAGER_XXSE_PLUGIN_NAME","test");
-    add_macro_var("XXSE_CSV_MANAGER_LOGGER_NAMESPACE","CsvManagerUse::NoLogger");
+-- target("xxse-CsvManager-header-dev")
+--     set_kind("headeronly")
+--     add_packages("rapidcsv")
+--     add_headerfiles("header/CsvManager.h")
+--     add_headerfiles("header/utils/*.h", {prefixdir = "utils"})
+--     local add_macro_var = function(name,value)
+--         add_defines(name.."="..value.."");
+--     end
+--     local add_macro_var_str = function(name,value)
+--         add_defines(name.."=\""..value.."\"");
+--     end
+--     add_macro_var_str("XXSE_CSV_MANAGER_XXSE_NAME","SFSE");
+--     add_macro_var_str("XXSE_CSV_MANAGER_XXSE_PLUGIN_NAME","test");
+--     add_macro_var("XXSE_CSV_MANAGER_LOGGER_NAMESPACE","CsvManagerUse::NoLogger");
 
 
